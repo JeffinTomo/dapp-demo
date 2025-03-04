eb753a5cf26e096ba7f84a249031fc0325d83789
水色 <xuezhengfeng@tomo.inc>
2025-03-04 20:28:10 +0800

    solana demo

--- src/solana-dapp.js
+++ src/solana-dapp.js
@@ -1,139 +1,161 @@
-// import React, { useState } from "react";
-// import {
-//   TransactionMessage,
-//   VersionedTransaction,
-//   SystemProgram,
-//   Connection,
-//   Transaction,
-// } from "@solana/web3.js";
+import React, { useState } from "react";
+import bs58 from "bs58";
+import {
+  TransactionMessage,
+  VersionedTransaction,
+  SystemProgram,
+  Connection,
+  Transaction,
+} from "@solana/web3.js";
 
-// const provider = window.bitkeep?.solana;
-
-// // https://web3.bitget.com/zh-CN/docs/provider-api/solana.html
+const provider = window.phantom?.solana;
 
 export default function SolanaDApp() {
-  //   const [connected, setConnected] = useState(false);
-  //   const [address, setAddress] = useState("");
-  //   const [signature, setSignature] = useState("");
-  //   const [transaction, setTransaction] = useState("");
-  //   const [signedTransaction, setSignedTransaction] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [address, setAddress] = useState("");
+  const [signature, setSignature] = useState("");
+  const [transaction, setTransaction] = useState("");
+  const [signedTransaction, setSignedTransaction] = useState("");
 
-  //   const connection = new Connection("https://api.devnet.solana.com/");
+  const connection = new Connection("https://api.devnet.solana.com/");
 
-  //   const connect = async () => {
-  //     if (!provider) return;
-  //     try {
-  //       await provider.connect();
-  //       const contractAddress = await provider.getAccount();
-  //       //  provider.publicKey.toString(); // Once the web application is connected to Bitkeep,
-  //       setConnected(true);
-  //       setAddress(contractAddress);
-  //     } catch {
-  //       setConnected(false);
-  //       alert("connected error");
-  //     }
-  //   };
+  const connect = async () => {
+    if (!provider) return;
+    try {
+      let { publicKey = "" } = (await provider.connect()) || {};
+      let address = publicKey?.toBase58();
+      console.log(publicKey.toString());
+      console.log(`Switched to account ${address}, publicKey: ${publicKey}`);
+      setConnected(true);
+      setAddress(address);
+    } catch (err) {
+      setConnected(false);
+      console.error("connected error", err);
+    }
+  };
 
-  //   const signMessage = async () => {
-  //     if (!connected) return;
-  //     try {
-  //       // uint8Array
-  //       const message = `You can use uint8array to verify`;
-  //       const encodedMessage = new TextEncoder().encode(message);
-  //       const signedMessage = await provider.signMessage(encodedMessage);
-  //       const signature = signedMessage?.signature;
-  //       setSignature(signature);
-  //     } catch (e) {
-  //       alert(e);
-  //     }
-  //   };
+  // const contractAddress = await provider.getAccount();
 
-  //   const signVersionedTransaction = async () => {
-  //     if (!provider) return;
+  const signMessage = async () => {
+    if (!connected) return;
+    try {
+      // uint8Array
+      const message = `You can use uint8array to verify`;
+      const encodedMessage = new TextEncoder().encode(message);
+      const signedMessage = await provider.signMessage(encodedMessage);
+      const signature = signedMessage?.signature;
+      setSignature(signature);
+    } catch (e) {
+      alert(e);
+    }
+  };
 
-  //     let minRent = await connection.getMinimumBalanceForRentExemption(0);
+  const signVersionedTransaction = async () => {
+    if (!provider) return;
 
-  //     // create array of instructions
-  //     const instructions = [
-  //       SystemProgram.transfer({
-  //         fromPubkey: provider.publicKey,
-  //         toPubkey: provider.publicKey,
-  //         lamports: minRent,
-  //       }),
-  //     ];
+    let minRent = await connection.getMinimumBalanceForRentExemption(0);
 
-  //     let blockhash = await connection
-  //       .getLatestBlockhash()
-  //       .then((res) => res.blockhash);
+    // create array of instructions
+    const instructions = [
+      SystemProgram.transfer({
+        fromPubkey: provider.publicKey,
+        toPubkey: provider.publicKey,
+        lamports: minRent,
+      }),
+    ];
 
-  //     // create v0 compatible message
-  //     const messageV0 = new TransactionMessage({
-  //       payerKey: provider.publicKey,
-  //       recentBlockhash: blockhash,
-  //       instructions,
-  //     }).compileToV0Message();
+    let blockhash = await connection
+      .getLatestBlockhash()
+      .then((res) => res.blockhash);
 
-  //     // make a versioned transaction
-  //     const transactionV0 = new VersionedTransaction(messageV0);
+    // create v0 compatible message
+    const messageV0 = new TransactionMessage({
+      payerKey: provider.publicKey,
+      recentBlockhash: blockhash,
+      instructions,
+    }).compileToV0Message();
 
-  //     const signedTransaction = await provider.signTransaction(transactionV0);
-  //     setTransaction(JSON.stringify(signedTransaction));
-  //   };
+    // make a versioned transaction
+    const transactionV0 = new VersionedTransaction(messageV0);
 
-  //   const signLegacyTransaction = async () => {
-  //     if (!provider) return;
+    const signedTransaction = await provider.signTransaction(transactionV0);
+    setTransaction(JSON.stringify(signedTransaction));
+  };
 
-  //     const transaction = new Transaction().add(
-  //       SystemProgram.transfer({
-  //         fromPubkey: provider.publicKey, //payer
-  //         toPubkey: provider.publicKey, //toAccount
-  //         lamports: 100,
-  //       }),
-  //     );
-  //     transaction.feePayer = provider.publicKey;
+  const signLegacyTransaction = async () => {
+    if (!provider) return;
 
-  //     const anyTransaction = transaction;
-  //     anyTransaction.recentBlockhash = (
-  //       await connection.getLatestBlockhash()
-  //     ).blockhash;
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: provider.publicKey, //payer
+        toPubkey: provider.publicKey, //toAccount
+        lamports: 100,
+      }),
+    );
+    transaction.feePayer = provider.publicKey;
 
-  //     const signedTransaction = await provider.signTransaction(transaction);
-  //     setSignedTransaction(JSON.stringify(signedTransaction));
-  //   };
-  //
-  return <>solana to do</>;
+    const anyTransaction = transaction;
+    anyTransaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
 
-  //   return (
-  //     <>
-  //       <h2>Solana Dapp Demo</h2>
+    const signedTransaction = await provider.signTransaction(transaction);
+    setSignedTransaction(JSON.stringify(signedTransaction));
+  };
 
-  //       <br />
-  //       <button onClick={connect}>connect</button>
-  //       <p>{connected ? "连接成功" : "请连接"}</p>
-  //       {address && <p>地址：{address}</p>}
-  //       <br />
+  return (
+    <div className="m-5 text-sm">
+      <h2>Solana Dapp Demo</h2>
 
-  //       <br />
-  //       <button onClick={signMessage}>signMessage</button>
-  //       {signature && (
-  //         <p style={{ width: "80%", wordBreak: "break-all" }}>
-  //           签名： {signature}
-  //         </p>
-  //       )}
-  //       <br />
+      <br />
+      <button onClick={connect} className="bg-[#000] text-[#fff] p-1 mb-2">
+        connect
+      </button>
+      <p className="bg-[#f5f5f5] border-1 p-5">
+        {connected ? "connected" : "not connected"}
+      </p>
+      {address && <p>地址：{address}</p>}
+      <br />
 
-  //       <br />
-  //       <button onClick={signVersionedTransaction}>
-  //         signVersionedTransaction
-  //       </button>
-  //       {transaction && <p>Versioned Transaction:{transaction}</p>}
-  //       <br />
+      <br />
+      <button onClick={signMessage} className="bg-[#000] text-[#fff] p-1 mb-2">
+        signMessage
+      </button>
+      {signature && (
+        <div
+          style={{ width: "80%", wordBreak: "break-all" }}
+          className="bg-[#f5f5f5] border-1 p-5"
+        >
+          签名： {signature}
+          <div>todo: verifyMessage</div>
+        </div>
+      )}
+      <br />
 
-  //       <br />
-  //       <button onClick={signLegacyTransaction}>signLegacyTransaction</button>
-  //       {signedTransaction && <p>Legacy Transaction:{signedTransaction}</p>}
-  //       <br />
-  //       <br />
-  //     </>
-  //   );
+      <br />
+      <button
+        onClick={signVersionedTransaction}
+        className="bg-[#000] text-[#fff] p-1 mb-2"
+      >
+        signVersionedTransaction
+      </button>
+      {transaction && <p>Versioned Transaction:{transaction}</p>}
+      <br />
+
+      <br />
+      <button
+        onClick={signLegacyTransaction}
+        className="bg-[#000] text-[#fff] p-1 mb-2"
+      >
+        signLegacyTransaction
+      </button>
+      {signedTransaction && (
+        <p className="bg-[#f5f5f5] border-1 p-5">
+          Legacy Transaction:{signedTransaction}
+        </p>
+      )}
+      <br />
+      <br />
+    </div>
+  );
 }
